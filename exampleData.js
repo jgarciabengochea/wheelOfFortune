3308e99021c00073251debeede1ab4ac94069003
Javi Garcia-Bengochea <jgb4410@gmail.com>
2019-01-10 13:52:17 -0800

    example data for wheel

---
+++ exampleData.js
@@ -0,0 +1,9 @@
+module.exports = {
+  exampleData: [
+    {value: 1, label: 'chinese'},
+    {value: 2, label: 'mexican'},
+    {value: 3, label: 'cuban'},
+    {value: 4, label: 'thai'},
+    {value: 5, label: 'italian'},
+  ],
+};
