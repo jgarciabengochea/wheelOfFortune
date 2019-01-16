55ab77028ffeac2c14ae63b3b79b3fd3088f90f5
Javi Garcia-Bengochea <jgb4410@gmail.com>
2019-01-16 11:25:32 -0800

    wheel rotates on every click

--- client/src/components/Wheel.jsx
+++ client/src/components/Wheel.jsx
@@ -3,7 +3,7 @@ import * as d3 from 'd3';
 import { exampleData } from './../../../exampleData.js';
 import styled, { keyframes } from 'styled-components';
 
-// TODO: this spins but not a random amount of degrees and thats making me mad
+// TODO: this spins a random amount of degrees but rarely will do a full rotation after first spin
 export default class Wheel extends Component {
   constructor(props) {
     super(props);
@@ -13,18 +13,22 @@ export default class Wheel extends Component {
     this.height = 400;
     this.width = 400;
     this.colors = d3.scaleOrdinal(d3.schemeAccent);
-    this.arc = d3.arc().innerRadius(0).outerRadius(100);
+    this.arc = d3.arc().innerRadius(10).outerRadius(100);
+    this.spinWheel = this.spinWheel.bind(this);
   }
+
   spinWheel(e) {
     this.setState({ spinning: true });
   }
+
   render() {
     const pie = d3.pie()([1, 1, 1, 1, 1]);
     const translate = `translate(${this.width / 2}, ${this.height / 2})`;
     const interpolate = d3.interpolateRgb('#eaaf79', '#bc3358');
+    const transform = `rotate(${(Math.random() * (360 * 5))}deg)`;
     return (
       <div>
-        <svg height={this.height} width={this.width} className={this.state.spinning ? 'wheel' : ''}>
+        <svg height={this.height} width={this.width} className='wheel' style={this.state.spinning ? { transform } : {}}>
           <g transform={translate}>
             {pie.map((data, index) => {
               const sliceColor = interpolate(index / (pie.length - 1));
@@ -32,7 +36,7 @@ export default class Wheel extends Component {
             })}
           </g>
         </svg>
-        <button onClick={this.spinWheel.bind(this)}>Spin</button>
+        <button onClick={this.spinWheel}>Spin</button>
       </div>
     );
   }
